43696e35cbe5e208df0f94f36a7874326b5d47dc
Annabelle Whipps <49943973+annabelle-whipps@users.noreply.github.com>
2022-01-18 16:21:59 +0000

    Update to error in myfile.js

--- myfile.js
+++ myfile.js
@@ -6,7 +6,7 @@ http.createServer(function(request, response) {
 	//HTTP Status: 200 : OK
 	//Content Type: text/plain 
 	
-	response.writeHead(200,{'Content-Type': ;text/plain'});
+	response.writeHead(200,{'Content-Type': 'text/plain'});
 	
 	//Sent the response body as "Hello World"
 	response.end ('Hello World\n');
